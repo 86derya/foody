41716d9c2ea6e548af7aa1b94472bbf64bc754fb
86derya <derya_86@ukr.net>
2018-12-05 20:08:25 +0400

    hw1 branch init

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import UserAuthorizationForm from './UserAuthorization';
+import Menu from './Menu';
+import Header from './App-Header';
+import DishList from '../data/DishList.json';
+import DishComment from './DishComment';
 import OrderHistory from './OrderHistory';
 import Orders from '../data/order-history.json';
 
-const App = () => <OrderHistory orders={Orders} />;
+const App = () => ( < div > < Header / > < hr / > < UserAuthorizationForm / > < hr / > < Menu dishList = {
+        DishList
+    }
+    /> < DishComment id = {
+    1
+}
+/>< hr / > < OrderHistory orders = {
+Orders
+}
+/ > </div > );
 export default App;
