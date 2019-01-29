bdb77d1edeb0e86ee01fabba924c977e5559b1ba
86derya <derya_86@ukr.net>
2019-01-29 17:37:47 +0200

    css edited _banan

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../configs/routes';
+
+import banan from '../data/banan.png';
 import styles from '../components/index.module.css';
 
 const NotFoundPage = () => (
-  <div>
+  <div className={styles.notFound_container}>
     <h1>Error 404</h1>
     <h2>Whoops...something went wrong</h2>
     <p>
@@ -15,11 +17,7 @@ const NotFoundPage = () => (
       </Link>
       to return to the menu page.
     </p>
-    <img
-      src="https://s3.amazonaws.com/campusreform/9679/BananaPeel.jpg"
-      alt="not_found"
-      width="600 px"
-    />
+    <img src={banan} alt="not_found" width="600 px" />
   </div>
 );
 export default NotFoundPage;
